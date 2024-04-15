71aaf9e6c078f49d419132e673cafb859df5f40d
wylu1037 <wylu1037@gmail.com>
2024-04-15 12:33:28 +0800

    chore: wallet

--- wallet/src/main/kotlin/com/example/wallet/model/Wallet.kt
+++ wallet/src/main/kotlin/com/example/wallet/model/Wallet.kt
@@ -1,8 +1,12 @@
 package com.example.wallet.model
 
+import com.example.crypto.extension.toECKeyPair
+import com.example.model.PrivateKey
+import com.example.wallet.newWallet
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
+import org.komputing.khex.model.HexString
 
 @Serializable
 data class Wallet(
@@ -12,9 +16,27 @@ data class Wallet(
     val isGM: Boolean
 ) {
     companion object {
-        fun fromJson(json: String): Wallet {
+        /**
+         * 从 FileKey 恢复钱包
+         *
+         * @param json FileKey 的JSON字符串
+         * @return 钱包
+         */
+        fun fromFileKey(json: String): Wallet {
             return Json.decodeFromString<Wallet>(json)
         }
+
+        /**
+         * 从 私钥 恢复钱包
+         *
+         * @param privateKeyHex 私钥(hex字符串)
+         * @param isGM sm2p256v1 or secp256k1
+         * @param password FileKey 密码
+         * @return 钱包
+         */
+        fun fromPrivateKey(privateKeyHex: String, isGM: Boolean = true, password: String) =
+            PrivateKey(HexString(privateKeyHex)).toECKeyPair(isGM).newWallet(password, isGM)
+
     }
 }
 
