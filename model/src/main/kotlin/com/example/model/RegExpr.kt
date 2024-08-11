900b2b8bb0de4485c5fbb643e89870f48a400cf1
wylu1037 <wylu1037@gmail.com>
2024-08-11 15:22:40 +0800

    chore: add require block

--- model/src/main/kotlin/com/example/model/RegExpr.kt
+++ model/src/main/kotlin/com/example/model/RegExpr.kt
@@ -5,10 +5,10 @@ object RegExpr {
     const val ADDRESS = "^zltc_[a-zA-Z0-9]{33}$"
 
     // 0x5f2be9a02b43f748ee460bf36eed24fafa109920
-    const val ETHEREUM_ADDRESS = "^0x[a-zA-Z]{40}$"
+    const val ETHEREUM_ADDRESS = "^(0x)?[a-zA-Z]{40}$"
 
     const val PRIVATE_KEY_HEX = "^0x[a-zA-Z0-9]{64}$"
-    
+
     // 压缩公钥
     const val COMPRESSED_PUBLIC_KEY_HEX = "^0x04[a-zA-Z0-9]{128}$"
 }
